8df49e47e21dd19151f0c8f53772a10e57916394
Jagjit Singh <jagjit.a.singh@capgemini.com>
2021-04-25 11:38:45 +0200

    Initial commit of logging POC

---
+++ AzureAppServiceLoggingPOC/AzureAppServiceLoggingPOC/LoggingApplication.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AzureAppServiceLoggingPOC
+{
+    // Dependency Injection of ILogger - Built In Provider & Logging of all LogLevels. 
+    public class LoggingApplication
+    {
+        private readonly ILogger _logger;
+        public LoggingApplication(ILogger<LoggingApplication> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogTrace()
+        {
+            _logger.LogTrace("Level 0 Initiated - Trace");
+        }
+
+        public void LogDebug()
+        {
+            _logger.LogDebug ("Level 1 Initiated - Debug");
+        }
+
+        public void LogInformation()
+        {
+            _logger.LogInformation("Level 2 Initiated - Information");
+        }
+        
+        public void LogWarning()
+        {
+            _logger.LogWarning("Level 3 Initiated - Warning");
+        }
+
+        public void LogError()
+        {
+            _logger.LogError("Level 4 Initiated - Error");
+        }
+
+        public void LogCritical()
+        {
+            _logger.LogError("Level 5 Initiated - Critical");
+        }
+    }
+}
---
+++ AzureAppServiceLoggingPOC/AzureAppServiceLoggingPOC/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace AzureAppServiceLoggingPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            LoggingApplication app = serviceProvider.GetService<LoggingApplication>();
+
+            // Initiale All Levels
+            app.LogTrace();
+            app.LogDebug();
+            app.LogInformation();
+            app.LogWarning();
+            app.LogError();
+            app.LogCritical();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole())
+            .AddTransient<LoggingApplication>();
+        }
+    }
+}
